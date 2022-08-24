ddb1fde2690c52a8ef3ace71a7ce7b321074e422
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2022-08-23 23:01:30 -0300

    Add initial structure to handle the products list

--- pages/orders/new.tsx
+++ pages/orders/new.tsx
@@ -6,7 +6,18 @@ import Layout from "app/core/layouts/Layout"
 import createOrder from "app/orders/mutations/createOrder"
 import { OrderForm, FORM_ERROR } from "app/orders/components/OrderForm"
 import SidebarLayout from "app/core/layouts/SidebarLayout"
-import { Box, Button, Card, CardHeader, Divider, Grid, IconButton, Typography } from "@mui/material"
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material"
 import { BoxCenter } from "app/core/components/BoxCenter"
 import { ArrowForwardTwoTone } from "@mui/icons-material"
 import EditIcon from "@mui/icons-material/Edit"
@@ -27,11 +38,36 @@ const NewOrderPage = () => {
         <h3>Cadastro de novo pedido</h3>
       </BoxCenter>
       <Grid container>
-        <Grid item xs={12}></Grid>
-        <Grid item xs={12} md={10}>
-          oi
+        <Grid item xs={12} md={8} lg={9} paddingBottom={1}>
+          <Box minWidth="100%">
+            <Grid container xs={12} spacing={1}>
+              {[0, 1, 2, 3, 4, 5, 6, 7].map((item, index) => (
+                <Grid item xs={12} sm={6} md={6} lg={4} xl={2} key={`produto-${index}`}>
+                  <Card>
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                      </Typography>
+                      {/*
+                                            <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread
+                      </Typography>
+                      */}
+                    </CardContent>
+                    <CardActions>
+                      <Box width="100%" display="flex" justifyContent="right">
+                        <Button size="small" color="success">
+                          Adicionar
+                        </Button>
+                      </Box>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
         </Grid>
-        <Grid item xs={12} md={2}>
+        <Grid item xs={12} md={4} lg={3}>
           <Card sx={{ minHeight: { xs: 0, md: 242 } }}>
             <CardHeader title="Resumo do Pedido" subheader={" saved addresses"} />
             <Divider />
